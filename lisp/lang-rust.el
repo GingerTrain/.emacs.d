6ab4c20a0c7d8ac706f0da8d05c398df46645053
Eric <tayloreric@rogers.com>
2019-07-16 01:01:59 -0400

    Remove comments

--- lisp/lang-rust.el
+++ lisp/lang-rust.el
@@ -1,16 +1,17 @@
 ;; -*- lexical-binding: t; -*-
 
-;; (use-package rustic
-;;   :ensure t
-;;   :commands rustic-mode
-;;   :config
-;;   (require 'lsp-mode)
-;;   (setq rustic-lsp-server     'rust-analyzer
-;;         rustic-format-on-save t))
+(use-package rustic
+:ensure t
+:commands rustic-mode
+:config
+(require 'lsp-mode)
+(setq rustic-lsp-server
+      'rust-analyzer
+      rustic-format-on-save t))
 
-;; (use-package flycheck-rust
-;;   :ensure t
-;;   :after (flycheck rustic-mode)
-;;   :hook (flycheck-mode . flycheck-rust-setup))
+(use-package flycheck-rust
+:ensure t
+:after (flycheck rustic-mode)
+:hook (flycheck-mode . flycheck-rust-setup))
 
 (provide 'lang-rust)
