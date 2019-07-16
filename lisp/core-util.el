a4151e166390f504f70f57bfa3a349152ab757ff
Eric <tayloreric@rogers.com>
2019-07-16 01:03:43 -0400

    Add yank-filename function

--- lisp/core-util.el
+++ lisp/core-util.el
@@ -64,4 +64,16 @@
   (interactive)
   (load-file (concat user-emacs-directory "init.el"))) ;; user-init-file ???
 
+(defun yank-filename ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+;; yank-current-filepath
+
 (provide 'core-util)
