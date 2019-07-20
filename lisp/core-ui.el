ce906f1306b0a1d43d0c0d5135c0250052f96bed
Eric <tayloreric@rogers.com>
2019-07-20 18:03:22 -0400

    Add rainbow delimiters, indentation highlighting, and emoji support

--- lisp/core-ui.el
+++ lisp/core-ui.el
@@ -10,12 +10,40 @@
 (set-default 'cursor-type 'box)  ;; Fat cursor
 (global-hl-line-mode t)          ;; Highlight current line
 (show-paren-mode)                ;; Show matching parentheses
+;;(fringe-mode '(4 . 0))           ;; Thinner fringes
 
 (setq-default
  mode-line-default-help-echo nil) ;; Disable mode-line hover tips
 
-(when (version<= "26.0.50" emacs-version ) ;; Enable line numbers
-      (global-display-line-numbers-mode ))
+(when (version<= "26.0.50" emacs-version )
+      (global-display-line-numbers-mode )) ;; Enable line numbers
+
+;; Indentation highlighting
+(use-package highlight-indent-guides
+  :ensure t
+  :hook (prog-mode . highlight-indent-guides-mode)
+  :config
+  (setq highlight-indent-guides-method 'character))
+
+;; Rainbow delimiters
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Emoji support
+(use-package company-emoji
+  :hook
+  ((markdown-mode . company-mode)
+   (git-commit-mode . company-mode))
+  :config
+  (add-to-list 'company-backends 'company-emoji))
+
+(use-package emojify
+  :hook
+  ((markdown-mode . emojify-mode)
+   (git-commit-mode . emojify-mode)
+   (magit-status-mode . emojify-mode)
+   (magit-log-mode . emojify-mode)))
 
 ;; Font
 (set-face-attribute 'default nil
