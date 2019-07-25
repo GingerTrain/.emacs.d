c8c914c2fe927a2980880bd62ef2a99aa9467ed6
Eric <tayloreric@rogers.com>
2019-07-24 21:43:05 -0400

    Add NeoTree

---
+++ lisp/pkg-neotree.el
@@ -0,0 +1,12 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package neotree
+  :ensure t
+  :bind ([f8] . neotree-toggle)
+  :after (all-the-icons)
+  :config
+  (setq neo-show-hidden-files t)
+  ;;(setq neo-autorefresh nil)
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+
+(provide 'pkg-neotree)
